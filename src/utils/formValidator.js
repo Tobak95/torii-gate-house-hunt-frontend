f6796a45204b372313898483630ec346aac8a181
tobak95 <akinola.tobi.2010@gmail.com>
2025-05-27 03:58:03 -0600

    renaming property schema according to the backend peoperty schema for effecitive validation

--- src/utils/formValidator.js
+++ src/utils/formValidator.js
@@ -4,7 +4,7 @@ export const registerSchema = Yup.object().shape({
   fullName: Yup.string().required("Name is required"),
   email: Yup.string().email("Invalid email").required("Email is required"),
   phoneNumber: Yup.string()
-    .matches(/^\+?[1-9][0-9]{7,14}$/ , "Phone number is not valid")
+    .matches(/^\+?[1-9][0-9]{7,14}$/, "Phone number is not valid")
     .required("Phone number is required"),
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
@@ -22,14 +22,15 @@ export const loginSchema = Yup.object().shape({
   password: Yup.string().required("Password is required"),
 });
 
+// Schema for creating or updating a property
 export const propertySchema = Yup.object().shape({
-  propertyName: Yup.string().required("Property name is required"),
+  title: Yup.string().required("Property name is required"),
   description: Yup.string().required("Description is required"),
   location: Yup.string().required("Location is required"),
-  rooms: Yup.string().required("Number of rooms is required"),
-  livingRooms: Yup.string().required("Number of living rooms is required"),
-  toilets: Yup.string().required("Number of toilets is required"),
-  kitchens: Yup.string().required("Number of kitchens is required"),
+  bedroom: Yup.string().required("Number of rooms is required"),
+  livingRoom: Yup.string().required("Number of living rooms is required"),
+  toilet: Yup.string().required("Number of toilets is required"),
+  kitchen: Yup.string().required("Number of kitchens is required"),
   price: Yup.string().required("Price is required"),
   paymentPeriod: Yup.string().required("Payment period is required"),
 });
@@ -40,7 +41,6 @@ export const forgotPasswordSchema = Yup.object().shape({
     .required("Email is required"),
 });
 
-
 export const resetPasswordSchema = Yup.object().shape({
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
@@ -48,4 +48,4 @@ export const resetPasswordSchema = Yup.object().shape({
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords must match")
     .required("Confirm password is required"),
-})
+});
